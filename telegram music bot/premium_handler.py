944259cabe554b534f5d99091f34b1c54d6eb44e
samakshei11-hue <samakshei11@gmail.com>
2025-08-30 01:48:24 +0530

    Delete telegram music bot directory

--- telegram music bot/premium_handler.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import filters
-from main import app  # Theek kiya: `app` ko main.py se import kiya gaya
-from modules.decorators import premium_only
-
-@app.on_message(filters.command("highqualityplay"))
-@premium_only
-async def high_quality_play(_, message):
-    await message.reply("Playing in CD quality!")
